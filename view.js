7804d95e09919c6e0d7138ad99f6ab873d3be527
Creciunel <cr.catalin@mail.ru>
2022-05-20 16:36:49 +0300

    Bootstrap design

--- view.js
+++ view.js
@@ -3,9 +3,13 @@ const elevRepo = require("./ElevRepo");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const e = require("express");
 
 const app = express();
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname , "static")))
 
 function Running() {
 	//Global variabile
@@ -18,89 +22,22 @@ function Running() {
 
 	//get
 	router.get("/", function (req, res) {
-		const form = `<form action="/" method="POST">
-        <table>
-            <thead>
-                <caption>
-                    <h3>Elev data base</h3>
-                </caption>
-            </thead>
-            <tbody>
-                <tr>
-                    <th><label for="1">Nume</label></th>
-                    <th><input type="text" name="name" /></th>
-                </tr>
-                <tr>
-                    <th><label for="2">Prenume </label></th>
-                    <th><input type="text" name="lastaName" /></th>
-                </tr>
-                <tr>
-                    <th><label for="2">Nota</label></th>
-                    <th><input type="text" name="nota" /></th>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th><input id="btn" type="submit" value="submit" /></th>
-                </tr>
-            </tfoot>
-        </table>
-    </form>
-    <p id="descript">Pagina este craeata de <b>Creciunel Catain<b> in scopul satisfacerii lucrarilor de laborator la disciplina Programarea avansata.</p>
-    <style>
-    table {
-        margin: 30% auto 5%;
-    }</style>`;
-
-		res.render("elevi", { form: form });
-		//res.sendFile(path.join(__dirname + "/index.html"));
+		res.render('index');
 	});
 
 	app.get("/list", (req, res) => {
-		let eLement = "";
-		for (let e of elevRepo.getAllData()) {
-			eLement +=
-				`<table><caption><h3>Input data</h3></caption>  <tbody><tr><th> Nume:</th> <th><a href="/list` +
-				e["id"] +
-				`">` +
-				e["nume"] +
-				`</th></tr>` +
-				`<tr> <th>Prenume: </th> <th>` +
-				e["prenume"] +
-				`</th></tr><tr><th> Nota: </th><th>` +
-				e["nota"] +
-				`</th></tr> </tbody></table>`;
-		}
-		eLement += `<a href="/">Home</a>`;
-        eLement += `<style> table {
-            margin: 3% auto 5%;
-        } </style> `;
-		res.render("elevi", { form: eLement });
+		res.render("list", { list: elevRepo.getAllData() });
 	});
 
 	app.get("/list:id", (req, res) => {
 		let idLoc = parseInt(req.params.id);
-		let eLement = "";
-		for (let e of elevRepo.getAllData()) {
-			if (e["id"] === idLoc) {
-				eLement +=
-					`<table><caption><h3>Input data</h3></caption>  <tbody><tr><th> Nume:</th> <th><a href="/list` +
-					e["id"] +
-					`">` +
-					e["nume"] +
-					`</th></tr>` +
-					`<tr> <th>Prenume: </th> <th>` +
-					e["prenume"] +
-					`</th></tr><tr><th> Nota: </th><th>` +
-					e["nota"] +
-					`</th></tr> </tbody></table>`;
+		console.log(idLoc);
+		
+		for(let e of elevRepo.getAllData()){
+			if(e.id == idLoc){
+				res.render("elev", { list: e });
 			}
 		}
-		eLement += `<a href="/">Home</a> <style>
-        table {
-            margin: 30% auto 5%;
-        }</style>`;
-		res.render("elevi", { form: eLement });
 	});
 
 	//post
@@ -111,25 +48,11 @@ function Running() {
 			parseInt(req.body.nota),
 			Index - 1
 		);
-
-		const resp =
-			`<table><caption><h3>Input data</h3></caption>  <tbody><tr><th> Nume:</th> <th>` +
-			req.body.name +
-			`</th></tr>` +
-			`<tr> <th>Prenume: </th> <th>` +
-			req.body.lastaName +
-			`</th></tr><tr><th> Nota: </th><th>` +
-			req.body.nota +
-			`</th></tr> </tbody></table> <a href="/">Home</a><br><a href="/list">List</a> <style>
-            table {
-                margin: 30% auto 5%;
-            }</style>`;
-
-		res.render("elevi", { form: resp });
 		elevRepo.add(Index, Elev);
 		Index += 1;
 		console.log(elevRepo.getAllData());
 		console.log("Nume " + Elev.nume);
+		res.render("list", { list: elevRepo.getAllData() });
 	});
 
 	app.use("/", router);
